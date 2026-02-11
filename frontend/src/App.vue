9d82daca901f1a9a2ea2aa9b309f737b948945d8
gabriela611 <gsanabriag@eafit.edu.co>
2026-02-10 23:46:54 -0500

    feat/sidebar buttons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,70 +4,132 @@ import { RouterLink, RouterView } from 'vue-router';
 
 
 <template>
-  <div class="bg-gray-100 min-h-screen">
+  <div class="bg-linear-to-t from-purple-950 to-purple-700 min-h-screen">
     <div class="flex h-screen overflow-hidden">
       <!-- sidebar -->
-      <aside class="w-64 bg-gray-800 text-white shadow-lg fixed h-full flex flex-col">
-        <div class="p-6 flex-1">
-          <div class="flex items-center mb-8">
-            <i class="fas fa-graduation-cap text-2xl mr-3 text-blue-400"></i>
-            <span class="font-bold text-xl">Dashboard</span>
-          </div>
-          <nav class="space-y-2">
-            <RouterLink to="/" class="flex items-center px-4 py-3 rounded-lg hover:bg-gray-700 transition duration-200">
-              <i class="fas fa-home mr-3"></i>
-              <span>Home</span>
-            </RouterLink>
-            <RouterLink to="/movies"
-              class="flex items-center px-4 py-3 rounded-lg hover:bg-gray-700 transition duration-200">
-              <i class="fas fa-film mr-3"></i>
-              <span>Movies</span>
-            </RouterLink>
-            <RouterLink to="/actresses"
-              class="flex items-center px-4 py-3 rounded-lg hover:bg-gray-700 transition duration-200">
-              <i class="fas fa-person mr-3"></i>
-              <span>Actresses</span>
-            </RouterLink>
-            <RouterLink to="/reviews"
-              class="flex items-center px-4 py-3 rounded-lg hover:bg-gray-700 transition duration-200">
-              <i class="fas fa-comment mr-3"></i>
-              <span>Reviews</span>
-            </RouterLink>
-          </nav>
-        </div>
-        <div class="w-full p-6 border-t border-gray-700 mt-auto">
-          <div class="flex items-center">
-            <div class="w-10 h-10 rounded-full bg-blue-500 flex items-center justify-center mr-3">
-              <i class="fas fa-user text-white"></i>
-            </div>
-            <div>
-              <p class="text-sm font-semibold">Admin User</p>
-              <p class="text-xs text-gray-400">admin@example.com</p>
-            </div>
-          </div>
-        </div>
-      </aside>
+      <!-- From Uiverse.io by icochran10 --> 
+<ul class="w-64 flex flex-col gap-1 border-l border-gray-200 pl-1 mt-70">
+
+    <!-- These elements will be converted to reusable components later :P-->
+
+    <!--Home button-->
+  <li 
+    class="group w-14 overflow-hidden rounded-lg border-l border-transparent bg-white transition-all duration-500 hover:w-64 hover:border-gray-200 hover:shadow-lg has-[:focus]:w-64 has-[:focus]:shadow-lg"
+  >
+    <RouterLink to="/"
+      class="peer flex w-full cursor-pointer items-center gap-2.5 px-3 py-2 text-left text-purple-800 transition-all active:scale-95"
+    >
+      <div class="rounded-lg border-2 border-purple-300 bg-purple-100 p-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="purple"
+          viewBox="0 0 512 512"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M240 6.1c9.1-8.2 22.9-8.2 32 0l232 208c9.9 8.8 10.7 24 1.8 33.9s-24 10.7-33.9 1.8l-8-7.2 0 205.3c0 35.3-28.7 64-64 64l-288 0c-35.3 0-64-28.7-64-64l0-205.3-8 7.2c-9.9 8.8-25 8-33.9-1.8s-8-25 1.8-33.9L240 6.1zm16 50.1L96 199.7 96 448c0 8.8 7.2 16 16 16l48 0 0-104c0-39.8 32.2-72 72-72l48 0c39.8 0 72 32.2 72 72l0 104 48 0c8.8 0 16-7.2 16-16l0-248.3-160-143.4zM208 464l96 0 0-104c0-13.3-10.7-24-24-24l-48 0c-13.3 0-24 10.7-24 24l0 104z"></path>
+        </svg>
+      </div>
+      <div class="font-semibold">Home</div>
+    </RouterLink>
+  
+</li>
+    <!--Movies button-->  
+<li
+    class="group w-14 overflow-hidden rounded-lg border-l border-transparent bg-white transition-all duration-500 hover:w-64 hover:border-gray-200 hover:shadow-lg has-[:focus]:w-64 has-[:focus]:shadow-lg"
+  >
+    <RouterLink to="/movies"
+      class="peer flex w-full cursor-pointer items-center gap-2.5 px-3 py-2 text-left text-purple-800 transition-all active:scale-95"
+    >
+      <div class="rounded-lg border-2 border-purple-300 bg-purple-100 p-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="purple"
+          viewBox="0 0 448 512"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            d="M0 96C0 60.7 28.7 32 64 32l320 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zM48 368l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm304-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zM48 240l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm304-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zM48 112l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16L64 96c-8.8 0-16 7.2-16 16zM352 96c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0z"
+            stroke-linejoin="round"
+            stroke-linecap="round"
+          ></path>
+        </svg>
+      </div>
+      <div class="font-semibold">Movies</div>
+    </RouterLink>
+
+  </li>
+    <!--Actress button-->
+  <li
+    class="group w-14 overflow-hidden rounded-lg border-l border-transparent bg-white transition-all duration-500 hover:w-64 hover:border-gray-200 hover:shadow-lg has-[:focus]:w-64 has-[:focus]:shadow-lg"
+  >
+    <RouterLink to="/actresses"
+      class="peer flex w-full cursor-pointer items-center gap-2.5 px-3 py-2 text-left text-purple-800 transition-all active:scale-95"
+    >
+      <div class="rounded-lg border-2 border-purple-300 bg-purple-100 p-1">
+        <svg
+          class="size-6"
+          stroke="currentColor"
+          stroke-width="1.5"
+          viewBox="0 0 384 512"
+          fill="purple"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M136 24a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zM112 384l-25.8 0c-10.9 0-18.6-10.7-15.2-21.1l43-129-48.3 65.1c-10.5 14.2-30.6 17.2-44.8 6.6s-17.2-30.6-6.6-44.8l70.5-95C110 132 149.7 112 192 112s82 20 107.2 53.9l70.5 95c10.5 14.2 7.6 34.2-6.6 44.8s-34.2 7.6-44.8-6.6L270 234 313 362.9c3.5 10.4-4.3 21.1-15.2 21.1l-25.8 0 0 128c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-128-32 0 0 128c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-128z"            
+            stroke-linejoin="round"
+            stroke-linecap="round"
+          ></path>
+        </svg>
+      </div>
+      <div class="font-semibold">Actress</div>
+    </RouterLink>
+    <div
+      class="grid grid-rows-[0fr] overflow-hidden transition-all duration-500 peer-focus:grid-rows-[1fr]"
+    >
+    </div>
+  </li>
+    <!--Reviews button-->
+  <li
+    class="group w-14 overflow-hidden rounded-lg border-l border-transparent bg-white transition-all duration-500 hover:w-64 hover:border-gray-200 hover:shadow-lg has-[:focus]:w-64 has-[:focus]:shadow-lg"
+  >
+    <RouterLink to="/reviews"
+      class="peer flex w-full cursor-pointer items-center gap-2.5 px-3 py-2 text-left text-purple-800 transition-all active:scale-95"
+    >
+      <div class="rounded-lg border-2 border-purple-300 bg-purple-100 p-1">
+        <svg
+          class="size-6"
+          stroke="currentColor"
+          stroke-width="1.5"
+          viewBox="0 0 576 512"
+          fill="purple"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M288.1-32c9 0 17.3 5.1 21.4 13.1L383 125.3 542.9 150.7c8.9 1.4 16.3 7.7 19.1 16.3s.5 18-5.8 24.4L441.7 305.9 467 465.8c1.4 8.9-2.3 17.9-9.6 23.2s-17 6.1-25 2L288.1 417.6 143.8 491c-8 4.1-17.7 3.3-25-2s-11-14.2-9.6-23.2L134.4 305.9 20 191.4c-6.4-6.4-8.6-15.8-5.8-24.4s10.1-14.9 19.1-16.3l159.9-25.4 73.6-144.2c4.1-8 12.4-13.1 21.4-13.1zm0 76.8L230.3 158c-3.5 6.8-10 11.6-17.6 12.8l-125.5 20 89.8 89.9c5.4 5.4 7.9 13.1 6.7 20.7l-19.8 125.5 113.3-57.6c6.8-3.5 14.9-3.5 21.8 0l113.3 57.6-19.8-125.5c-1.2-7.6 1.3-15.3 6.7-20.7l89.8-89.9-125.5-20c-7.6-1.2-14.1-6-17.6-12.8L288.1 44.8z"         
+            stroke-linejoin="round"
+            stroke-linecap="round"
+          ></path>
+        </svg>
+      </div>
+      <div class="font-semibold">Reviews</div>
+    </RouterLink>
+    <div
+      class="grid grid-rows-[0fr] overflow-hidden transition-all duration-500 peer-focus:grid-rows-[1fr]"
+    >
+    </div>
+  </li>
+
+</ul>
+
       <!-- main content area -->
       <div class="flex-1 flex flex-col overflow-hidden ml-64">
-        <!-- top header -->
-        <header class="bg-white shadow-sm border-b border-gray-200">
-          <div class="px-6 py-4 flex items-center justify-between">
-            <div>
-              <h1 class="text-2xl font-bold text-gray-800">
-                {{ $route.meta.title }}
-              </h1>
-            </div>
-            <div class="flex items-center space-x-4">
-              <button class="p-2 text-gray-600 hover:bg-gray-100 rounded-lg transition duration-200">
-                <i class="fas fa-search"></i>
-              </button>
-              <div
-                class="w-10 h-10 rounded-full bg-blue-500 flex items-center justify-center cursor-pointer hover:bg-blue-600 transition duration-200">
-                <i class="fas fa-user text-white"></i>
-              </div>
-            </div>
-          </div>
-        </header>
         <!-- main content -->
         <main class="flex-1 overflow-y-auto p-6">
           <RouterView />
